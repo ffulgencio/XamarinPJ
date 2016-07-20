1a8667f690a0403ea8fa13f796082defefbd379b
Francis Fulgencio <francisfulgencio@gmail.com>
2016-07-20 13:58:25 -0400

    firstcommit

---
+++ PasarDatosActivity/PasarDatosActivity/Activity2.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using System;
+
+namespace PasarDatosActivity
+{
+	[Activity(Label = "Activity2")]
+	public class Activity2 : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			String texto = Intent.GetStringExtra("Mensaje") ?? "No se encontro el texto";
+
+
+
+			// Create your application here
+			SetContentView(Resource.Layout.Activity2);
+
+			TextView mensajeRecib = FindViewById<TextView>(Resource.Id.tvMensaje);
+			mensajeRecib.Text=texto;
+
+			Button btnVolver = FindViewById<Button>(Resource.Id.btnVolver);
+
+			btnVolver.Click += delegate 
+			{
+				var intento = new Intent(this, typeof(MainActivity));
+				StartActivity(intento);
+				Finish();
+			};
+			                     
+		}
+	}
+}
+
---
+++ PasarDatosActivity/PasarDatosActivity/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace PasarDatosActivity
+{
+	[Activity(Label = "PasarDatosActivity", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		//int count = 1;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+			EditText mensaje = FindViewById<EditText>(Resource.Id.edMensaje);
+
+			button.Click += delegate 
+			{
+				//button.Text = string.Format("{0} clicks!", count++); 
+				var activity2 = new Intent(this, typeof(Activity2));
+			
+
+				activity2.PutExtra("Mensaje", mensaje.Text);
+
+				StartActivity(activity2);
+
+				Finish();
+
+			};
+		}
+	}
+}
+
+
